882a022eae6e333b1dd0ad00c82db54a5e870608
Olivier Nadeau <oliviernadeau2003@gmail.com>
2024-11-11 15:03:32 -0500

    .

--- src/TP02_1170_NumB_A24.java
+++ src/TP02_1170_NumB_A24.java
@@ -9,8 +9,10 @@ import java.util.List;
 public class TP02_1170_NumB_A24 {
     public static void AfficherNumB() {
         List<Pays> paysList = new ArrayList<>();
-        int nombrePays = lireFichierPays("data/pays_a24.txt", paysList);
+        String currentDirectory = System.getProperty("user.dir");
+        int nombrePays = lireFichierPays(currentDirectory + "/src/data/pays_a24.txt", paysList);
         System.out.println("Nombre de pays lus : " + nombrePays);
+
     }
 
     public static int lireFichierPays(String fichier, List<Pays> paysList) {
@@ -19,20 +21,34 @@ public class TP02_1170_NumB_A24 {
         try (BufferedReader br = new BufferedReader(new FileReader(fichier))) {
             String ligne;
             while ((ligne = br.readLine()) != null) {
-                String[] elements = ligne.split(","); // Assuming comma-separated values
-                char continent = elements[0].charAt(0); // First character is continent code
-                String nom = elements[1];
-                String capitale = elements[2];
-                double superficie = Double.parseDouble(elements[3]);
+                String[] elements = ligne.trim().split("\\s+");
 
-                Pays pays = new Pays(continent, nom, capitale, superficie);
+                char continent = elements[0].charAt(0);
+                String capitale = elements[elements.length - 3];
+                double superficie = Double.parseDouble(elements[elements.length - 2]);
+                long population = Long.parseLong(elements[elements.length - 1]);
+
+                StringBuilder nomBuilder = new StringBuilder();
+                for (int i = 0; i < elements.length - 3; i++) {
+                    nomBuilder.append(elements[i]);
+                    if (i < elements.length - 4) {
+                        nomBuilder.append(" ");
+                    }
+                }
+                String nom = nomBuilder.toString().substring(1);
+
+                Pays pays = new Pays(continent, nom, capitale, superficie, population);
                 paysList.add(pays);
                 nombrePays++;
             }
         } catch (IOException e) {
             System.err.println("Erreur de lecture du fichier: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.err.println("Erreur de format dans le fichier: " + e.getMessage());
         }
 
         return nombrePays;
     }
+
 }
+
--- src/classes/Pays.java
+++ src/classes/Pays.java
@@ -5,13 +5,35 @@ public class Pays {
     private String nom;
     private String capitale;
     private double superficie;
+    private long population;
 
-    public Pays(char continent, String nom, String capitale, double superficie) {
+    public Pays(char continent, String nom, String capitale, double superficie, long population) {
         this.continent = continent;
         this.nom = nom;
         this.capitale = capitale;
         this.superficie = superficie;
+        this.population = population;
     }
 
-    // Getters, setters
+    // Getters
+    public char getContinent() {
+        return continent;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getCapitale() {
+        return capitale;
+    }
+
+    public double getSuperficie() {
+        return superficie;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+
 }
